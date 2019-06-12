439899bed52aa3491654c22eb4242f0df78fdd44
ciec-20171106182 <43473342+ciec-20171106182@users.noreply.github.com>
2019-06-12 21:47:52 +0800

    行走动画和待机动画的控制

--- ActRpg/Assets/PlayerInput.cs
+++ ActRpg/Assets/PlayerInput.cs
@@ -11,6 +11,14 @@ public class PlayerInput : MonoBehaviour
 
     public float Dup;
     public float Dright;
+
+    public bool inputEnabled=true;
+
+    private float targetDup;
+    private float targetDright;
+    private float velocityDup;
+    private float velocityDright;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +28,16 @@ public class PlayerInput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Dup = (Input.GetKey(KeyUp) ? 1.0f : 0) - (Input.GetKey(KeyDown) ? 1.0f : 0);
-        Dright = (Input.GetKey(KeyRight) ? 1.0f : 0) - (Input.GetKey(KeyLeft) ? 1.0f : 0);
+        targetDup = (Input.GetKey(KeyUp) ? 1.0f : 0) - (Input.GetKey(KeyDown) ? 1.0f : 0);
+        targetDright = (Input.GetKey(KeyRight) ? 1.0f : 0) - (Input.GetKey(KeyLeft) ? 1.0f : 0);
 
+        if (inputEnabled==false)
+        {
+            targetDright = 0;
+            targetDup = 0;
+        }
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);//调用SmoothDamp对上下旗帜进行衰减处理，移动更平滑。
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
 
     }
 }
